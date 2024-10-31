628ae98c7da89dcb35162aaca96eb9ae69836a23
nikushabitskinashvili <130066334+nikushabitskinashvili@users.noreply.github.com>
2024-10-31 18:29:31 +0400

    feat: make login and login with google

--- src/shared/components/OAuthLogin/OAuthLogin.tsx
+++ src/shared/components/OAuthLogin/OAuthLogin.tsx
@@ -1,22 +1,28 @@
+
 'use client';
-import { StaticImport } from 'next/dist/shared/lib/get-img-props';
-import Image from 'next/image';
-import { ComponentType } from 'react';
-import styles from './OAuthLogin.module.scss';
-import { oAuthLogin } from '@/src/app/(auth)/utils/oAuthLogin/oAuthLogin';
-import { OAuthTypeEnum } from '@/src/shared/types/enums/oauth-type.enum';
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import { OAuthTypeEnum } from "@/src/shared/types/enums/oauth-type.enum";
+import styles from "./OAuthLogin.module.scss";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 interface Props {
   type: OAuthTypeEnum;
   image: StaticImport;
 }
 
-const OAuthLogin: ComponentType<Props> = (props: Props) => {
+const OAuthLogin: React.FC<Props> = ({ type, image }) => {
+  const handleOAuthLogin = () => {
+    if (type === OAuthTypeEnum.Google) {
+      signIn("google");
+    } 
+  };
+
   return (
     <Image
-      onClick={() => oAuthLogin(props.type)}
-      src={props.image}
-      alt={'oauth login icon'}
+      onClick={handleOAuthLogin}
+      src={image}
+      alt="oauth login icon"
       width={30}
       height={30}
       className={styles.image}
